2a4f2fce417b6cf66529cb8434781729210b2cac
hieule <lehieu1@hpcc.msu.edu>
2020-06-29 14:28:31 -0400

    Evaluate only every nb_files, nb_workers=0

--- gnn_icecube/multi_main.sh
+++ gnn_icecube/multi_main.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=deyoungbuyin
 #SBATCH --job-name=IceCube_GNN
 #SBATCH --output=/mnt/scratch/lehieu1/log/GNN/GNN_%A_%a.out
-#SBATCH --time=1-23:59:00
-#SBATCH --gres=gpu:k80:1
+#SBATCH --time=23:59:00
+#SBATCH --gres=gpu:v100:1
 #SBATCH --nodes=1
 #SBATCH --mem=50G
 #SBATCH --mail-type=FAIL # notifications for job fail
@@ -25,12 +25,12 @@ NB_TEST=100000
 # Experiment
 export SLURM_TIME_FORMAT='%m%d%y'
 DATE=$(squeue -j ${SLURM_JOB_ID} -o "%V")
-NAME="${DATE: -6}_500gev_mincuts_10x100k_20patience"
+NAME="${DATE: -6}_test"
 RUN="$SLURM_ARRAY_TASK_ID"
 PROJECT="HPCC"
 
 PATIENCE=20
-NB_EPOCH=200
+NB_EPOCH=50
 LRATE=0.05
 BATCH_SIZE=32
 
@@ -42,16 +42,16 @@ NB_HIDDEN=64
 NB_TRAIN=`expr ${NB_TRAIN} / ${NB_FILE}`
 PATIENCE=`expr ${PATIENCE} \* ${NB_FILE}`
 NB_EPOCH=`expr ${NB_EPOCH} \* ${NB_FILE}`
-TRAINFILE=${TRAINFILE[@]:0:${NB_FILE}}
+TRAINFILE_SLICED=${TRAINFILE[@]:0:${NB_FILE}}
 
 # Entering arguments
-PYARGS="--name $NAME --run $RUN --project $PROJECT --train_file ${TRAINFILE[@]} --val_file $VALFILE --test_file $TESTFILE $OPTIONS --nb_train $NB_TRAIN --nb_val $NB_VAL --nb_test $NB_TEST --batch_size $BATCH_SIZE --nb_epoch $NB_EPOCH --lrate $LRATE --patience $PATIENCE --nb_layer $NB_LAYER --nb_hidden $NB_HIDDEN"
+PYARGS="--name $NAME --run $RUN --project $PROJECT --train_file ${TRAINFILE_SLICED[@]} --val_file $VALFILE --test_file $TESTFILE $OPTIONS --nb_train $NB_TRAIN --nb_val $NB_VAL --nb_test $NB_TEST --batch_size $BATCH_SIZE --nb_epoch $NB_EPOCH --lrate $LRATE --patience $PATIENCE --nb_layer $NB_LAYER --nb_hidden $NB_HIDDEN"
 
 echo -e "\nStarting experiment with name $NAME...\n"
 
 module load powertools
 source /mnt/home/lehieu1/anaconda3/etc/profile.d/conda.sh
-conda activate /mnt/home/lehieu1/load_conda_env
+conda activate /mnt/ufs18/home-105/lehieu1/load_conda_env/GNN_conda
 
 time python /mnt/home/lehieu1/IceCube/code/GNN/gnn_icecube/src/multi_main.py $PYARGS
 
--- gnn_icecube/src/multi_main.py
+++ gnn_icecube/src/multi_main.py
@@ -93,25 +93,26 @@ def train(
                                   args,
                                   experiment_dir,
                                   train_loader)
-    val_stats = evaluate(net, criterion, experiment_dir, args,
-                            valid_loader, 'Valid')
-                                
-    utils.track_epoch_stats(i, args.lrate, 0, train_stats, val_stats, experiment_dir)
+    if (((i+1) % len(multi_train_loader)) == 0):
+      val_stats = evaluate(net, criterion, experiment_dir, args,
+                            valid_loader, 'Valid')                          
+      utils.track_epoch_stats(i, args.lrate, 0, train_stats, val_stats, experiment_dir)
 
-    # Update learning rate, remaining nb epochs to train
-    scheduler.step(val_stats[0])
+      # Update learning rate, remaining nb epochs to train
+      scheduler.step(val_stats[0])
+
+      # Track best model performance
+      if (val_stats[0] > args.best_tpr):
+        logging.warning("Best performance on valid set.")
+        args.best_tpr = float(val_stats[0])
+        utils.update_best_plots(experiment_dir)
+        utils.save_best_model(experiment_dir, net)
+        utils.save_best_scores(i, val_stats[2], val_stats[0], val_stats[1], experiment_dir)
+        utils.save_epoch_model(experiment_dir, net)
+
     args.lrate = optimizer.param_groups[0]['lr']
     args.nb_epochs_complete += 1
 
-    # Track best model performance
-    if (val_stats[0] > args.best_tpr):
-      logging.warning("Best performance on valid set.")
-      args.best_tpr = float(val_stats[0])
-      utils.update_best_plots(experiment_dir)
-      utils.save_best_model(experiment_dir, net)
-      utils.save_best_scores(i, val_stats[2], val_stats[0], val_stats[1], experiment_dir)
-
-    utils.save_epoch_model(experiment_dir, net)
     utils.save_args(experiment_dir, args)
     logging.info("Epoch took {} seconds.".format(int(time.time()-t0)))
     
